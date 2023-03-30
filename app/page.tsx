ac1897495883087a48cf0ac5970af0d449f8ba88
neilien <neil.laravel@gmail.com>
2023-03-30 11:05:49 +0800

    added hello

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 export default function Home() {
   return (
-    <main>
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Welcome&nbsp;
+        <span className="whitespace-nowrap">to <span className="font-bold">YummyDhez Baked Goods</span>
+
+        </span>
+      </p>
 
     </main>
   )
